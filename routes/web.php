80ee198c0eaa759a9775aaf7788aa85ff89b5884
kolyayurev <kolyayurew@gmail.com>
2023-12-23 18:45:58 +0300

    feat: breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
+});
+
+require __DIR__.'/auth.php';
